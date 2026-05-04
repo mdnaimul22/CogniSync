26a26ac635d074f13dd79dc15d20b27248d80917
mdnaimul22 <engineer.naimul@gmail.com>
2026-05-04 18:41:59 +0600

    Fix: export missing file utilities in config init

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,6 +2,7 @@ from .paths import PROJECT_ROOT, find_project_root
 from .files import (
     read_text, write_text, read_json, write_json,
     exists, ensure_dir, delete, list_files, get_abs_path,
+    get_size, read_from_pos,
 )
 from .dotenv import load_dotenv, set_value, get_value, remove_value
 from .settings import Settings
@@ -13,6 +14,8 @@ __all__ = [
     "PROJECT_ROOT", "find_project_root",
     "read_text", "write_text", "read_json", "write_json",
     "exists", "ensure_dir", "delete", "list_files", "get_abs_path",
+    "get_size", "read_from_pos",
     "load_dotenv", "set_value", "get_value", "remove_value",
     "Settings", "setup_logger",
 ]
+
